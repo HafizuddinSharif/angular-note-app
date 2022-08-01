0d30971ebdc0214eb938c5b485ec01e320643cc4
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2022-08-01 11:21:55 +0800

    Intial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TextEditorComponent } from './components/text-editor/text-editor.component';
+import { ListNotesComponent } from './components/list-notes/list-notes.component';
+import { NoteComponent } from './components/note/note.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TextEditorComponent,
+    ListNotesComponent,
+    NoteComponent
   ],
   imports: [
     BrowserModule
