10381f8b756e7203048255be30111110db179eff
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2022-08-08 14:54:48 +0800

    Add text-editor component

--- src/app/components/home/text-editor/text-editor.component.ts
+++ src/app/components/home/text-editor/text-editor.component.ts
@@ -1,15 +1,110 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+import { Note } from 'src/app/state/models/note.model';
+import { add_note, update_note } from 'src/app/state/actions/note.actions';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/state/app.state';
+import { getNoteCount, selectNote } from 'src/app/state/selectors/note.selector';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-text-editor',
   templateUrl: './text-editor.component.html',
   styleUrls: ['./text-editor.component.scss']
 })
-export class TextEditorComponent implements OnInit {
+export class TextEditorComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  editorForm: FormGroup
+  content: string
+  id: number;
+  title: string
+  newNote: Note
+
+  randomText: string = "This is a text"
+
+  @Input() idSelected: number
+
+  @Output() editorClose = new EventEmitter()
+  @Output() latestNoteId = new EventEmitter()
+
+
+  constructor(private store: Store<AppState>) {
+    store.select(getNoteCount).subscribe(count => this.id = count + 1);
+    // console.log("The number produced is: ", this.id)
+  }
 
   ngOnInit(): void {
+    this.fillEditor()
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.fillEditor()
+  }
+
+  onSubmit() {
+
+    this.content = this.editorForm.get('editor')!.value
+    this.title = this.editorForm.get('title')!.value
+
+    // console.log(this.editorForm.get('editor')!.value)
+
+    if (this.content == ""  || this.title == "") {
+      // console.log("No content!")
+      return
+    }
+
+    this.newNote = {
+      id: this.idSelected > 0 ? this.idSelected : this.id,
+      user_id: 10,
+      date: new Date(),
+      status: "idle",
+      title: this.title,
+      content: this.content
+    }
+
+    if (this.idSelected > 0) {
+      this.store.dispatch(update_note({note: this.newNote}))
+      this.latestNoteId.emit(this.idSelected)
+      // console.log("This is invoked")
+    } else {
+      this.store.dispatch(add_note({note: this.newNote}))
+      this.latestNoteId.emit(this.id)
+    }
+
+    this.editorClose.emit(false)
+  }
+
+  // Helper function for this class
+  private fillEditor() {
+
+    if (this.idSelected > 0) {
+      this.store.select(selectNote(this.idSelected)).subscribe( note => {
+
+        if (note) {
+          this.editorForm = new FormGroup({
+            'editor': new FormControl(note.content),
+            'title': new FormControl(note.title)
+          })
+
+        } else {
+
+          this.editorForm = new FormGroup({
+            'editor': new FormControl(""),
+            'title': new FormControl(""),
+          })
+
+        }
+      })
+    } else {
+      this.editorForm = new FormGroup({
+        'editor': new FormControl(""),
+        'title': new FormControl(""),
+      })
+    }
+    
+    this.content = ""
+
   }
 
 }
