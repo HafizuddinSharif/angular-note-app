0e4ba2b506d0bf3e653c2a086461af4495958308
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2022-08-08 14:55:35 +0800

    Add note component

--- src/app/components/home/note/note.component.ts
+++ src/app/components/home/note/note.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Note } from 'src/app/state/models/note.model';
+import { AppState } from 'src/app/state/app.state';
+import { selectAllNotes, selectNote } from 'src/app/state/selectors/note.selector';
 
 @Component({
   selector: 'app-note',
@@ -7,9 +13,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoteComponent implements OnInit {
 
-  constructor() { }
+  notes: Observable<Note[]>
+  found: Observable<Note>
+
+  @Output() openNoteId = new EventEmitter()
+  @Output() deleteNoteId = new EventEmitter()
+
+  constructor(private store: Store<AppState>) {
+    this.notes = this.store.select(selectAllNotes)
+  }
 
   ngOnInit(): void {
   }
 
+  getNoteId(id: any) {
+    this.openNoteId.emit(id);
+  }
+
+  getDeleteNoteId(id: any) {
+    this.deleteNoteId.emit(id)
+  }
 }
+ 
