1eb62479fceaa8e2d6bfd9f0736bd00ba4fa1b35
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2022-08-08 14:57:32 +0800

    Add home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @Input() isEditorOpen2: boolean = true
+  @Input() idSelected: number
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  receiveData(value: any) {
+    this.isEditorOpen2 = value
+  }
+
+  viewNote(value: any) {
+    this.idSelected = value
+  }
+
+  getNoteId(obj: any) {
+
+    if (obj.option === 1) {
+      this.idSelected = obj.note_id
+      this.isEditorOpen2 = false
+    }
+
+    if (obj.option === 2) {
+      this.idSelected = obj.note_id
+      this.isEditorOpen2 = true
+      console.log("✏️ Edit note...")
+    }
+
+    
+  }
+
+  openEditor(id: any) {
+    this.isEditorOpen2 = true
+    this.idSelected = 0
+  }
+
+  getDeleteNoteId(id: any) {
+
   }
 
 }
