a6c054f9a442d97cdf3f755e90c3062f64b7c6ba
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2022-08-10 08:26:23 +0800

    Change login state management to ngrx

--- src/app/guards/login.guard.ts
+++ src/app/guards/login.guard.ts
@@ -1,16 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 
 
 import { AuthService } from '../services/auth/auth.service';
+import { AppState } from '../state/app.state';
+import { User } from '../state/models/user.model';
+import { getUser } from '../state/selectors/user.selector';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {}
+   user: User
+
+  constructor(private authService: AuthService, private router: Router, private store: Store<AppState>) {
+
+   store.select(getUser).subscribe(
+      user => this.user = user
+   )
+
+  }
   
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -22,9 +34,10 @@ export class LoginGuard implements CanActivate {
   }
 
   checkLogin(url: string): true | UrlTree {
-     let val: any = localStorage?.getItem('isUserLoggedIn');
 
-     if(val != null && val == "true") {
+   //   let val: any = localStorage?.getItem('isUserLoggedIn');
+
+     if(this.user != null && this.user.id > 0) {
         if(url == "/login")
            return this.router.parseUrl('/home');
         else
--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -5,13 +5,27 @@ import { tap, delay } from 'rxjs/operators';
 
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/state/app.state';
-import { login_user } from 'src/app/state/actions/user.actions';
+import { login_user, logout_user } from 'src/app/state/actions/user.actions';
 import { User } from 'src/app/state/models/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthService {
+
+  users: User[] =[
+    {
+      id: 1,
+      username: 'admin1',
+      password: 'admin1'
+    },
+    {
+      id: 2,
+      username: 'admin2',
+      password: 'admin2'
+    }
+  ]
 
   isUserLoggedIn: boolean = false;
   currUser: User
@@ -19,33 +33,35 @@ export class AuthService {
   constructor(private store: Store<AppState>) {}
 
   login(userName: string, password: string): Observable<Boolean> {
-    this.isUserLoggedIn = userName == 'admin' && password == 'admin';
-    localStorage.setItem('isUserLoggedIn', this.isUserLoggedIn ? "true" : "false");
 
-    if (this.isUserLoggedIn) {
+    const foundUser = this.users.find(user => user.username == userName)
 
-      this.currUser = {
-        id: 1,
-        username: userName,
-        password: password
+    if (foundUser) {
+
+      this.isUserLoggedIn = password == foundUser.password;
+      // localStorage.setItem('isUserLoggedIn', this.isUserLoggedIn ? "true" : "false");
+
+      if (this.isUserLoggedIn) {
+        this.store.dispatch(login_user({user: foundUser}))
+        console.log('Log in successful')
       }
 
-      this.store.dispatch(login_user({user: this.currUser}))
     }
-
+    
     return of(this.isUserLoggedIn).pipe(
         delay(1000),
         tap(val => { 
-          console.log("Is User Authentication is successful: " + val); 
-          console.log(localStorage)
+          // console.log("Is User Authentication is successful: " + val); 
+          // console.log(localStorage)
         })
     );
   }
 
   logout(): void {
     this.isUserLoggedIn = false;
-    localStorage.removeItem('isUserLoggedIn'); 
-    console.log(localStorage)
+    // localStorage.removeItem('isUserLoggedIn');
+
+    this.store.dispatch(logout_user());
   }
  
 }
