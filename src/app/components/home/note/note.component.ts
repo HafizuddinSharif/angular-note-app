abc325e1ccb5176e134b0eefba84df8f782801c8
Hafizuddin Sharif <hafizuddinshariff@gmail.com>
2022-08-10 08:33:23 +0800

    Render list of note based on user id

--- src/app/components/home/note/note.component.ts
+++ src/app/components/home/note/note.component.ts
@@ -4,7 +4,9 @@ import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
 import { Note } from 'src/app/state/models/note.model';
 import { AppState } from 'src/app/state/app.state';
-import { selectAllNotes, selectNote } from 'src/app/state/selectors/note.selector';
+import { getNoteByUserId, selectAllNotes, selectNote } from 'src/app/state/selectors/note.selector';
+import { getUser } from 'src/app/state/selectors/user.selector';
+import { User } from 'src/app/state/models/user.model';
 
 @Component({
   selector: 'app-note',
@@ -15,12 +17,19 @@ export class NoteComponent implements OnInit {
 
   notes: Observable<Note[]>
   found: Observable<Note>
+  user: Observable<User>
 
   @Output() openNoteId = new EventEmitter()
   @Output() deleteNoteId = new EventEmitter()
 
   constructor(private store: Store<AppState>) {
-    this.notes = this.store.select(selectAllNotes)
+
+    this.store.select(getUser).subscribe(
+      user => {
+        this.notes = this.store.select(getNoteByUserId(user.id))
+      }
+    )
+    
   }
 
   ngOnInit(): void {
